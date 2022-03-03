1a3a9fba122d17642d4b353138a3e6e686e46d6f
Rofer11607 <rafac@monsterrg.com>
2022-03-03 15:49:59 -0500

    testbed for new components

---
+++ src/app/pages/test-page/test-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import * as interfaces from '../../../../interfaces.d';
+import * as services from '../../services';
+services.fixNeverReadError(interfaces);
+
+/*****Description*****
+
+this is a page that allows for testing of individual components
+********************/
+
+@Component({
+  selector: 'rafa-test-page',
+  templateUrl: './test-page.component.html',
+  styleUrls: ['./test-page.component.scss'],
+})
+export class TestPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
