c50a9cb6c39a5f264cffa41c54f666812fadf382
Rofer11607 <rafac@monsterrg.com>
2022-03-04 16:27:26 -0500

    inital

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as interfaces from '../../../../interfaces.d';
 import * as services from '../../services';
 services.fixNeverReadError(interfaces);
@@ -16,6 +16,24 @@ like us on the right
   styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
+  links: Array<interfaces.Link_> = [
+    {
+      text: 'Website',
+      url: 'https://www.monsterrg.com',
+    },
+    {
+      text: 'Facebook',
+      url: 'https://www.facebook.com/MRGVacations/',
+    },
+    {
+      text: 'Instagram',
+      url: 'https://www.instagram.com/MRGVacations/',
+    },
+    {
+      text: 'About',
+      url: 'https://monsterrg.com/blog/',
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
